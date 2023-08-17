163219128eae8123f3e952e3e21acc2fbe0f0a65
Asger Gitz-Johansen <agj@hmkbilcon.dk>
2023-08-17 21:34:51 +0200

    docs: add javadocs to everything

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # yadi
-Yet another framework-agnostic skyhook dependency injection implementation for Java applications
+Yet another framework-agnostic skyhook dependency injection implementation for Java applications.
+
+This library is a fairly simple global store where you can store object instances and `Supplier<T>`'s.
+Even though it is simple, it can be very powerful.
+
+## Usage
+Say you have an interfaced class called `MyFoo` like so:
+
+```java
+interface IFooable {
+    void foo();
+}
+
+class MyFoo implements IFooable {
+    @Override
+    public void foo() {
+        // Do a thing
+    }
+}
+```
+
+Then you would inject the instance at program start like so:
+
+```java
+public class Main {
+    public static void main(String[] args) {
+        DI.add(IFooable.class, new MyFoo());
+        DI.lock(); // Do not allow any other place in the application to add more stuff to DI (optional)
+        // start your application
+    }
+}
+
+// ... further inside the application code ...
+
+public class SomeService {
+    private final IFooable injectedFooInstance;
+
+    public SomeService() {
+        this.injectedFooInstance = DI.get(IFooable.class);
+    }
+}
+```
+
+
--- src/java/main/dk/yalibs/yadi/DI.java
+++ src/java/main/dk/yalibs/yadi/DI.java
@@ -6,39 +6,36 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.Supplier;
 
-import dk.gtz.graphedit.exceptions.NotFoundException;
-import dk.gtz.graphedit.view.util.IAction;
-
 /**
- * TODO:
+ * A static store of object instances and supplier functions.
  */
 public class DI {
-    private static Logger logger = LoggerFactory.getLogger(DI.class);
     private static Map<Class<?>,Object> dependencies = new HashMap<>();
     private static Map<Class<?>,Supplier<Object>> suppliers = new HashMap<>();
     private static Map<String,Object> namedDependencies = new HashMap<>();
     private static Map<String,Supplier<Object>> namedSuppliers = new HashMap<>();
-    private static Map<Class<?>,List<IAction>> onAddActions = new HashMap<>();
     private static boolean isLocked = false;
 
+    /**
+     * Add an object to the store.
+     * Note that if a key is added twice, it will simply overwrite the old store entry.
+     * @param <T>
+     * @param key class key, used for lookup later
+     * @param obj the object instance to store
+     */
     public static <T> void add(Class<? extends T> key, T obj) {
+        if(isLocked)
+            throw new DIAddException("Cannot add supplier with key '%s' because DI store has been locked".formatted(key.toString()));
         dependencies.put(key, obj);
-        if(onAddActions.containsKey(key))
-            onAddActions.get(key).forEach(a -> {
-                try {
-                    a.run(obj);
-                } catch(Exception e) {
-                    logger.error(e.getMessage(), e);
-                }
-            });
     }
 
     /**
-     * Add a supplier function to the global store.
+     * Add a supplier function to the store.
      * The supplier function will be called every time a resource with the appropriate key is requested.
+     * Note that if a key is added twice, it will simply overwrite the old store entry.
      * @param <T>
-     * @param key
-     * @param supplier
+     * @param key class key used for lookup later
+     * @param supplier function that can create objects
      */
     public static <T> void add(Class<? extends T> key, Supplier<? extends T> supplier) {
         if(isLocked)
@@ -47,10 +44,12 @@ public class DI {
     }
 
     /**
-     * TODO:
+     * Add a named object to the store.
+     * It is recommended to use {@link #add(Class, Object) add} instead, since strings can be unreliable and difficult to keep track of
+     * Note that if a key is added twice, it will simply overwrite the old store entry.
      * @param <T>
-     * @param key
-     * @param obj
+     * @param key string name of the object
+     * @param obj the object instance to store
      */
     public static <T> void add(String key, T obj) {
         if(isLocked)
@@ -59,8 +58,11 @@ public class DI {
     }
 
     /**
-     * @param key
-     * @param supplier
+     * Add a named supplier function to the store
+     * It is recommended to use {@link #add(Class, Supplier) add} instead, since strings can be unreliable and difficult to keep track of
+     * Note that if a key is added twice, it will simply overwrite the old store entry.
+     * @param key string name of the object
+     * @param supplier function that can create objects
      */
     public static void add(String key, Supplier<Object> supplier) {
         if(isLocked)
@@ -69,29 +71,51 @@ public class DI {
     }
 
     /**
-     * Locks the global store so no more additions can be done. This may be useful to enforce read-only from some point on
+     * Locks the global store so no more additions can be done. This may be useful to enforce read-only from some point on.
      * Note that {@link DI} does not provide an 'unlock' feature, so be mindful of when you lock the store.
      */
     public static void lock() {
         isLocked = true;
     }
 
-    public static <T> T get(Class<? super T> key) throws NotFoundException {
+    /**
+     * Get an injected object based off of a class key.
+     * The object will be freshly created if a supplier is associated with the key.
+     * @param <T>
+     * @param key the key to lookup in the store
+     * @return the object from the store associated with the key
+     * @throws NullPointerException is thrown if the key is not available in the store
+     */
+    public static <T> T get(Class<? super T> key) throws NullPointerException {
         if(dependencies.containsKey(key))
             return (T)dependencies.get(key);
         if(suppliers.containsKey(key))
             return (T)suppliers.get(key).get();
-        throw new NotFoundException("DI unable to resolve class '%s'".formatted(key.getName()));
+        throw new NullPointerException("DI unable to resolve class '%s'".formatted(key.getName()));
     }
 
-    public static <T> T get(String key) throws NotFoundException {
+    /**
+     * Get an injected object based off a name key.
+     * The object will be freshly created if a supplier is associated with the key.
+     * @param <T>
+     * @param key
+     * @return
+     * @throws NullPointerException
+     */
+    public static <T> T get(String key) throws NullPointerException {
         if(namedDependencies.containsKey(key))
             return (T)namedDependencies.get(key);
         if(namedSuppliers.containsKey(key))
             return (T)namedSuppliers.get(key).get();
-        throw new NotFoundException("DI unable to resolve named dependency '%s'".formatted(key));
+        throw new NullPointerException("DI unable to resolve named dependency '%s'".formatted(key));
     }
 
+    /**
+     * Checks if the store contains the provided class key
+     * @param <T>
+     * @param key class key of the object
+     * @return true if either an appropriate object instance, or a supplier has been injected
+     */
     public static <T> boolean contains(Class<? super T> key) {
         if(dependencies.containsKey(key))
             return true;
@@ -100,6 +124,11 @@ public class DI {
         return false;
     }
 
+    /**
+     * Checks if the store contains the provided name key
+     * @param key string name of the object
+     * @return true if either an appropriate object instance, or a supplier has been injected
+     */
     public static boolean contains(String key) {
         if(namedDependencies.containsKey(key))
             return true;
@@ -107,12 +136,5 @@ public class DI {
             return true;
         return false;
     }
-
-    public static <T> void onAdd(Class<? extends T> key, IAction action) {
-        if(!onAddActions.containsKey(key))
-            onAddActions.put(key, new ArrayList<IAction>());
-        onAddActions.get(key).add(action);
-    }
 }
-
 
